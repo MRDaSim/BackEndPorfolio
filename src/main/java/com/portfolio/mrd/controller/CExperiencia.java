b76082081db9391c3ec34b9efa5977cde31566d4
MRDaSim <danialbrecht9@gmail.com>
2022-08-03 19:50:46 -0300

    direcsiones front

--- src/main/java/com/portfolio/mrd/controller/CExperiencia.java
+++ src/main/java/com/portfolio/mrd/controller/CExperiencia.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("explab")
-@CrossOrigin(origins = "https://front-ap.web.app")
+@CrossOrigin(origins = "http://front-ap.web.app")
 public class CExperiencia {
 
     @Autowired

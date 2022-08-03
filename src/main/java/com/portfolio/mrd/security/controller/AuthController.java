798c3587120f97ca7b5d4b99cd5f62262b84a338
MRDaSim <danialbrecht9@gmail.com>
2022-08-03 20:51:18 -0300

    direcsiones front

--- src/main/java/com/portfolio/mrd/security/controller/AuthController.java
+++ src/main/java/com/portfolio/mrd/security/controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin (origins = "https://hosting-angular-proyecto-ap.web.app")
 public class AuthController {
 
     @Autowired
